bd5095bb39f58eaac796f60eb60a9dbe1d9c6455
danialmkj <danialmalekij@yahoo.com>
2023-04-06 17:39:23 +0430

    edit : sign up cubit

--- lib/features/feature_auth/presentation/bloc/sign_up_cubit/sign_up_cubit.dart
+++ lib/features/feature_auth/presentation/bloc/sign_up_cubit/sign_up_cubit.dart
@@ -1,12 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:store_app/features/feature_auth/repository/auth_repository.dart';
 
 import '../../../../../common/params/sign_up_params.dart';
+import '../../../data/models/login_with_sms_model.dart';
 import '../../../data/models/sign_up_model.dart';
 
 part 'signup_data_status.dart';
 part 'sign_up_state.dart';
+part 'call_code_status.dart';
 
 class SignUpCubit extends Cubit<SignupState> {
-  SignUpCubit() : super(SignupState(signUpDataStatus: SignUpDataLoading()));
+  AuthRepository authRepository;
+
+  SignUpCubit(this.authRepository)
+      : super(SignupState(
+            signUpDataStatus: SignUpDataLoading(),
+            callCodeStatus: CallCodeLoading()));
+
+
+
+ //write methods   
+
 }
--- lib/features/feature_auth/presentation/bloc/sign_up_cubit/sign_up_state.dart
+++ lib/features/feature_auth/presentation/bloc/sign_up_cubit/sign_up_state.dart
@@ -2,16 +2,20 @@ part of 'sign_up_cubit.dart';
 
 class SignupState {
   SignUpDataStatus signUpDataStatus;
+  CallCodeStatus callCodeStatus;
 
   SignupState({
     required this.signUpDataStatus,
+    required this.callCodeStatus,
   });
 
   SignupState copyWith({
     SignUpDataStatus? newSignUpDataStatus,
+    CallCodeStatus? newCallCodeStatus,
   }){
     return SignupState(
       signUpDataStatus: newSignUpDataStatus ?? signUpDataStatus,
+      callCodeStatus: newCallCodeStatus ?? callCodeStatus,
     );
   }
 }
