c6c1be27e708bdf15b0d99acdebf9a9edbef01ea
danialmkj <danialmalekij@yahoo.com>
2023-04-07 14:41:57 +0430

    edit : login cubit

--- lib/features/feature_auth/presentation/bloc/login_cubit/login_cubit.dart
+++ lib/features/feature_auth/presentation/bloc/login_cubit/login_cubit.dart
@@ -1,11 +1,63 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
+import 'package:store_app/common/resources/data_state.dart';
+import 'package:store_app/features/feature_auth/repository/auth_repository.dart';
+import '../../../data/models/code_model.dart';
 import '../../../data/models/login_with_sms_model.dart';
 
 part 'login_state.dart';
 part 'login_data_status.dart';
+part 'code_check_status.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  AuthRepository authRepository;
+  LoginCubit(this.authRepository) : super(LoginState(loginDataStatus: LoginDataInitial(), codeCheckStatus: CodeCheckInitial()));
+
+
+  // method Load Login Sms
+  Future<void> LoadLoginSms(phoneNumber)async{
+    
+    //emit Loading
+    emit(state.copyWith(newLoginDataStatus: LoginDataLoading()));
+
+
+    DataState dataState = await authRepository.fetchLoginWithSMS(phoneNumber);
+
+    if (dataState is DataSuccess) {
+
+      //emit complete
+      emit(state.copyWith(newLoginDataStatus: LoginDataCompleted(dataState.data)));
+    } else if(dataState is DataFailed){
+      
+      //emit error
+      emit(state.copyWith(newLoginDataStatus: LoginDataError(dataState.error!)));
+    }
+  }
+
+
+
+  // method Load Code Check
+  Future<void> LoadCodeCheck(code)async{
+    
+    //emit Loading
+    emit(state.copyWith(newCodeCheckStatus: CodeCheckLoading()));
+
+
+    DataState dataState = await authRepository.fetchCodeCheckData(code);
+
+    if (dataState is DataSuccess) {
+
+      //emit complete
+      emit(state.copyWith(newCodeCheckStatus: CodeCheckCompleted(dataState.data)));
+    } else if(dataState is DataFailed){
+      
+      //emit error
+      emit(state.copyWith(newCodeCheckStatus: CodeCheckError(dataState.error!)));
+    }
+  }
+
+
+
+
+
 }
--- lib/features/feature_auth/presentation/bloc/login_cubit/login_state.dart
+++ lib/features/feature_auth/presentation/bloc/login_cubit/login_state.dart
@@ -1,6 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'login_cubit.dart';
 
-@immutable
-abstract class LoginState {}
+class LoginState {
+  final LoginDataStatus loginDataStatus;
+  final CodeCheckStatus codeCheckStatus;
+  
+  LoginState({
+    required this.loginDataStatus,
+    required this.codeCheckStatus,
+  });
 
-class LoginInitial extends LoginState {}
+
+  LoginState copyWith({
+    LoginDataStatus? newLoginDataStatus,
+    CodeCheckStatus? newCodeCheckStatus,
+  }) {
+    return LoginState(
+      loginDataStatus: newLoginDataStatus ?? loginDataStatus,
+      codeCheckStatus: newCodeCheckStatus ?? codeCheckStatus,
+    );
+  }
+}
+
