017ef9a73664391e4df8f115c67b71768ac40941
danialmkj <danialmalekij@yahoo.com>
2023-04-07 15:27:39 +0430

    edit : change screen

--- lib/common/widgets/main_wrapper.dart
+++ lib/common/widgets/main_wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app/common/widgets/bottom_nav.dart';
 import 'package:store_app/features/feature_product/presentation/screens/category_screen.dart';
 
@@ -6,6 +7,7 @@ import '../../features/feature_home/presentation/screens/home_screen.dart';
 import '../../features/feature_product/presentation/widgets/searchbox_widget.dart';
 import '../../features/feature_product/repository/all_product_repository.dart';
 import '../../locator.dart';
+import '../blocs/bottom_nav_cubit/bottom_nav_cubit.dart';
 
 class MainWrapper extends StatelessWidget {
   MainWrapper({super.key});
@@ -29,39 +31,51 @@ class MainWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNav(controller: pageController),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 10,),
-
-            /// search Box
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 2,
-                        color: Colors.grey.shade400,
-                        offset: const Offset(0, 3)
-                    )
-                  ]
+        bottomNavigationBar: BottomNav(controller: pageController),
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10.0,right: 10, bottom: 10),
-                child: SearchTextField(controller: searchController, allProductsRepository: locator<AllProductsRepository>(),),
-              ),
-            ),
-            const SizedBox(height: 10,),
 
-          Expanded(
-            child: PageView(
-              controller: pageController,
-              children: topLevelScreen,
-            ),
+              /// search Box
+              Container(
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      blurRadius: 2,
+                      color: Colors.grey.shade400,
+                      offset: const Offset(0, 3))
+                ]),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 10.0, right: 10, bottom: 10),
+                  child: SearchTextField(
+                    controller: searchController,
+                    allProductsRepository: locator<AllProductsRepository>(),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+
+              Expanded(
+                child: PageView(
+                  controller: pageController,
+                  children: topLevelScreen,
+                  onPageChanged: (value) {
+                    /// change selected index
+                    BlocProvider.of<BottomNavCubit>(context)
+                        .changeSelectedIndex(value);
+                    pageController.animateToPage(value,
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut);
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'shopping app',
-      home: MobileLoginScreen(),
+      initialRoute:'/',
       routes: {
         SplashScreen.routeName: (context) => const SplashScreen(),
         IntroMainWrapper.routeName: (context) => IntroMainWrapper(),
